f376f03c4c3c5572ceff34d86bc847d273ac4b02
kymharriesdawisespiritu <codingjourney@gmail.com>
2024-08-22 15:15:22 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, counted] = useState(0);
+  const increment = () => {
+    counted(count + 1);
+  };
+  const decrement = () => {
+    counted(count - 1);
+  };
+  return (
+    <div className="App">
+      <button onClick={increment}>take a count </button>
+      <button onClick={decrement}>less a count</button>
+      <p>{count}</p>
+    </div>
+  );
+}
+
+export default App;
